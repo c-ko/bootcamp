8f5ca46a0d57422c085cbc7c17151e411f388ef8
Catherine Ko <catherineko@Catherines-MacBook-Air.local>
2020-07-28 18:55:22 -0700

    Commit of Bootcamp materials through day 5

---
+++ zen.py
@@ -0,0 +1,7 @@
+# Print some more stuff
+#THIS WAS A TXT FILE CHANGED TO .PY FILE
+
+print('Beautiful is better than ugly.')
+print('Explicit is better than implicit.') # want to be clear 
+print('Simple is better than complex.') # want to be as simple as possible 
+print('Complex is better than complicated.') # complex at least elegant
